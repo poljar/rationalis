e6e73a6e8f18c1f55d78aa7b1195bbbf319e5315
poljar (Damir Jelić) <poljar@termina.org.uk>
2017-07-20 17:49:36 +0200

    Rework command line parsing.

--- app/Main.hs
+++ app/Main.hs
@@ -26,7 +26,6 @@ import qualified Network.Wreq.Session as S
 
 import qualified Data.ByteString.Lazy as B
 
-
 getJSON :: FilePath -> IO B.ByteString
 getJSON jsonFile = B.readFile jsonFile
 
@@ -36,31 +35,32 @@ getJSON jsonFile = B.readFile jsonFile
 -- URL for account data via json
 -- https://net.pbz.hr/pbz365/accountTurnovers/currentAccTurnovers.json
 
-data Options = Options
-    { period :: Period
-    , json   :: FilePath
-    }
-
 periodReader :: ReadM (Period)
 periodReader = eitherReader $ \arg ->
-    case parseMaybe periodParser arg of
-      Nothing -> Left $ trace(arg) $ ("Cannot parse date: " ++ arg)
-      Just period -> Right period
+      case parseMaybe periodParser arg of
+          Nothing -> Left ("Cannot parse date: " ++ arg)
+          Just period -> Right period
+
+periodOption = option periodReader
+              ( long "period"
+                 <> short 'p'
+                 <> metavar "PERIOD"
+                 <> help "Fetch transactions only for the given time-period."
+              )
 
 -- TODO period should be mandatory only for network fetching
+{-
 argparse :: Parser Options
-argparse = Options
-      <$> option periodReader
-          ( long "period"
-         <> short 'p'
-         <> metavar "PERIOD"
-         <> help "Fetch transactions only for the given time-period." )
-      <*>
-          strOption
-          ( long "json-file"
-         <> short 'j'
-         <> metavar "JSON"
-         <> help "JSON file to use instead of fetching it automatically." )
+argparse = Options <$>
+    periodOption
+    <*>
+    strOption
+    ( long "json-file"
+        <> short 'j'
+        <> metavar "JSON"
+        <> help "JSON file to use instead of fetching it automatically."
+    )
+-}
 
 data Transaction = Transaction
     { date        :: Day
@@ -102,16 +102,45 @@ filterTransactions jsonData = jsonData ^.. members . key "result" . members .
                 key "currencyCode" . _String & T.unpack)
         )
 
-main :: IO ()
-main = parser =<< execParser opts
-  where
-    opts = info (argparse <**> helper)
-      ( fullDesc
-     <> progDesc "Fetch transaction data and convert it to ledger transactions."
-     <> header "bankfetcher - a bank transaction fetcher" )
+data Command
+    = Fetch Period
+    | Convert FilePath
 
-parser :: Options -> IO ()
-parser (Options p j) = do
-    putStrLn $ "Hello " ++ show p
-    jsonData <- getJSON j
-    mapM_ printTransaction $ filterTransactions jsonData
+data Options = Options Command
+
+main :: IO ()
+main = run =<< execParser opts
+    where
+        opts = info (parseOptions <**> helper)
+            ( fullDesc
+           <> progDesc "Fetch transaction data and convert it to ledger transactions."
+           <> header "bankfetcher - a bank transaction fetcher"
+            )
+
+withInfo :: Parser a -> String -> ParserInfo a
+withInfo opts desc = info (helper <*> opts) $ progDesc desc
+
+parseFetch :: Parser Command
+parseFetch = Fetch
+    <$> periodOption
+
+{-
+parseFetch :: Parser Command
+parseFetch = Convert
+    <$> periodOption
+-}
+
+parseCommand :: Parser Command
+parseCommand = subparser $
+    command "fetch"     (parseFetch   `withInfo`
+        "Fetch transaction data from the server.")
+--    command "convert"   (parseConvert `withInfo`
+--        "Convert a json file containing transaction data to ledger entries")
+
+parseOptions :: Parser Options
+parseOptions = Options <$> parseCommand
+
+run :: Options -> IO ()
+run (Options cmd) = do
+    case cmd of
+      Fetch period -> putStrLn "fetch"
