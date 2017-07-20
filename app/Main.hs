9ce564442631de09b52445c1a28b0b730ad5cb57
poljar (Damir Jelić) <poljar@termina.org.uk>
2017-07-20 18:00:34 +0200

    Refactoring: Move Transaction handling to the lib.

--- app/Main.hs
+++ app/Main.hs
@@ -4,13 +4,9 @@ module Main where
 import Lib
 
 import Data.String
-import Control.Lens
-import Data.Aeson.Lens
 
-import Data.Maybe
 import Data.Time
 import Data.Time.Format
-import Data.Scientific
 import Data.Tuple
 import Control.Monad
 import Control.Monad.IO.Class
@@ -20,7 +16,6 @@ import Options.Applicative
 import Control.Applicative
 import Data.Semigroup ((<>))
 
-import qualified Data.Text as T
 import qualified Network.Wreq as Request
 import qualified Network.Wreq.Session as S
 
@@ -47,60 +42,6 @@ periodOption = option periodReader
                  <> metavar "PERIOD"
                  <> help "Fetch transactions only for the given time-period."
               )
-
--- TODO period should be mandatory only for network fetching
-{-
-argparse :: Parser Options
-argparse = Options <$>
-    periodOption
-    <*>
-    strOption
-    ( long "json-file"
-        <> short 'j'
-        <> metavar "JSON"
-        <> help "JSON file to use instead of fetching it automatically."
-    )
--}
-
-data Transaction = Transaction
-    { date        :: Day
-    , description :: String
-    , payAmount   :: Maybe Float
-    , recAmount   :: Maybe Float
-    , currency    :: String
-    } deriving (Show)
-
--- TODO amount shold come from either pay or rec and payee and payer should be
--- swapped depending on pay or rec
--- TODO indentation should be based on the lengths of the payer/payee accounts
--- TODO replace ??? using regex based rules
-printTransaction :: Transaction -> IO ()
-printTransaction (Transaction day description pay rec currency) = do
-    putStrLn descriptionLine
-    putStrLn payeeLine
-    putStrLn payerLine
-        where descriptionLine = d ++ " * " ++ description
-              d = formatTime defaultTimeLocale "%Y/%m/%d" day
-              payeeLine = indent ++ "Expenses:???" ++ indent ++ (show $ fromJust amount)
-              payerLine = indent ++ "Assets:PBZ" ++ indent ++ " -" ++ (show $ fromJust amount)
-              indent = "    "
-              amount = pay
-
-parseDate :: T.Text -> Day
-parseDate s = parseTimeOrError True defaultTimeLocale "%d.%m.%Y. %T" $ T.unpack s
-
--- TODO ^?! aborts if it can't get the value
-filterTransactions :: B.ByteString -> [Transaction]
-filterTransactions jsonData = jsonData ^.. members . key "result" . members .
-    key "bankAccountTransactionList" . _Array .
-    traverse . to (\t -> Transaction
-        ( t ^?! key "currencyDate" . _String & parseDate)
-        ( t ^?! key "description" . _String & T.unpack)
-        ( t ^?  key "payAmount" . key "amount" . _Number & fmap toRealFloat)
-        ( t ^?  key "receiveAmount" . key "amount" . _Number & fmap toRealFloat)
-        ( t ^?! key "amountAfterTransaction" . key "currency" .
-                key "currencyCode" . _String & T.unpack)
-        )
 
 data Command
     = Fetch Period
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib
     ( parseMaybe
     , periodParser
     , Period
+    , Transaction
+    , filterTransactions
+    , printTransaction
     ) where
+
+import Data.Maybe
+import Data.Aeson.Lens
+import Data.Scientific
 
 import Data.Time
 import Data.Time.Clock
 import Data.Time.Calendar
+
+import Control.Lens
 import Control.Applicative
+import qualified Data.Text as T
+import qualified Data.ByteString.Lazy as B
 import qualified Text.ParserCombinators.ReadP as RP
 
 type Period = (Day, Day)
@@ -66,3 +78,44 @@ parseMaybe parser input =
     case RP.readP_to_S parser input of
         [] -> Nothing
         ((result, _):_) -> Just result
+
+
+data Transaction = Transaction
+    { date        :: Day
+    , description :: String
+    , payAmount   :: Maybe Float
+    , recAmount   :: Maybe Float
+    , currency    :: String
+    } deriving (Show)
+
+-- TODO amount shold come from either pay or rec and payee and payer should be
+-- swapped depending on pay or rec
+-- TODO indentation should be based on the lengths of the payer/payee accounts
+-- TODO replace ??? using regex based rules
+printTransaction :: Transaction -> IO ()
+printTransaction (Transaction day description pay rec currency) = do
+    putStrLn descriptionLine
+    putStrLn payeeLine
+    putStrLn payerLine
+        where descriptionLine = d ++ " * " ++ description
+              d = formatTime defaultTimeLocale "%Y/%m/%d" day
+              payeeLine = indent ++ "Expenses:???" ++ indent ++ (show $ fromJust amount)
+              payerLine = indent ++ "Assets:PBZ" ++ indent ++ " -" ++ (show $ fromJust amount)
+              indent = "    "
+              amount = pay
+
+parseDate :: T.Text -> Day
+parseDate s = parseTimeOrError True defaultTimeLocale "%d.%m.%Y. %T" $ T.unpack s
+
+-- TODO ^?! aborts if it can't get the value
+filterTransactions :: B.ByteString -> [Transaction]
+filterTransactions jsonData = jsonData ^.. members . key "result" . members .
+    key "bankAccountTransactionList" . _Array .
+    traverse . to (\t -> Transaction
+        ( t ^?! key "currencyDate" . _String & parseDate)
+        ( t ^?! key "description" . _String & T.unpack)
+        ( t ^?  key "payAmount" . key "amount" . _Number & fmap toRealFloat)
+        ( t ^?  key "receiveAmount" . key "amount" . _Number & fmap toRealFloat)
+        ( t ^?! key "amountAfterTransaction" . key "currency" .
+                key "currencyCode" . _String & T.unpack)
+        )
